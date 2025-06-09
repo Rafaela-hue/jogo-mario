11dc51b1a04473bb60f000a4d588ba533d450667
Rafaela-hue <rafaela.marques.freitas@escola.pr.gov.br>
2025-06-09 07:42:12 -0300

    .

--- main.js
+++ main.js
@@ -1,2 +1,24 @@
 const mario = document.querySelector(".mario")
 const pipe = document.querySelector(".pipe")
+
+const jump = () => {
+    mario.classList.add("jump");
+    setTimeout(() => {
+        mario.classList.remove
+    }
+    ,500);
+}
+
+const loop = setInterval(() => {
+const pipePosition = pipe.offsetleft;
+const marioPosition = +window.getComputedStyle(mario).bottom.replace("px", "");
+
+   if(pipePosition <= 120 && pipePosition > 0 && marioPosition <80 ) {
+     pipe.style.animation = "none";
+    pipe.style.left = `$(pipePosition)px` ;
+
+    mario.style.animation = "none";
+    mario.style.left = `$(marioPosition)px` ;
+   }
+})
+document.addEventListener("keydown, jump");
